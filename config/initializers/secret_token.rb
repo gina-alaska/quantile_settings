e517bfc9b365a9003a926e5fb7a9c4f683a63ad3
Will Fisher <will@teknofire.net>
2012-09-19 12:55:27 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuantileMapping::Application.config.secret_token = '35bb0944df5d803dfd6b0d5aacb2376c63d39fe346f139ab4d1126d56d3e028e14ae19d24e0d87cd2be35995138c19cede4f9c37920adbe1d4d120e81ccee7a9'
