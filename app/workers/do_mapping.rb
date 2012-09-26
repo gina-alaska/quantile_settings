90cd03c5e5d83ef2e4d96acc66ee230f485edd6b
Will Fisher <will@teknofire.net>
2012-09-26 14:28:42 -0800

    fixing more typos of historical

--- app/workers/do_mapping.rb
+++ app/workers/do_mapping.rb
@@ -7,7 +7,7 @@ class DoMapping
     settings.start
     # do the stuff to run the command here
     script = '/home/jiang/projects/quantilemapping/v3/quantile_mapping_auto.py'
-    cmd = [script, settings.historial_start, settings.historical_end, settings.predicted_start, settings.predicted_end, settings.period.name, settings.algorithm]
+    cmd = [script, settings.historical_start, settings.historical_end, settings.predicted_start, settings.predicted_end, settings.period.name, settings.algorithm]
     #script.py 1910 2001 2030 2060 Yearly 0
     
     puts "Running: #{cmd.join(' ')}"
--- test/functional/quantile_settings_controller_test.rb
+++ test/functional/quantile_settings_controller_test.rb
@@ -18,7 +18,7 @@ class QuantileSettingsControllerTest < ActionController::TestCase
 
   test "should create quantile_setting" do
     assert_difference('QuantileSetting.count') do
-      post :create, quantile_setting: { algorithm: @quantile_setting.algorithm, historial_start: @quantile_setting.historial_start, historical_end: @quantile_setting.historical_end, period_id: @quantile_setting.period_id, predicted_end: @quantile_setting.predicted_end, predicted_start: @quantile_setting.predicted_start }
+      post :create, quantile_setting: { algorithm: @quantile_setting.algorithm, historical_start: @quantile_setting.historical_start, historical_end: @quantile_setting.historical_end, period_id: @quantile_setting.period_id, predicted_end: @quantile_setting.predicted_end, predicted_start: @quantile_setting.predicted_start }
     end
 
     assert_redirected_to quantile_setting_path(assigns(:quantile_setting))
@@ -35,7 +35,7 @@ class QuantileSettingsControllerTest < ActionController::TestCase
   end
 
   test "should update quantile_setting" do
-    put :update, id: @quantile_setting, quantile_setting: { algorithm: @quantile_setting.algorithm, historial_start: @quantile_setting.historial_start, historical_end: @quantile_setting.historical_end, period_id: @quantile_setting.period_id, predicted_end: @quantile_setting.predicted_end, predicted_start: @quantile_setting.predicted_start }
+    put :update, id: @quantile_setting, quantile_setting: { algorithm: @quantile_setting.algorithm, historical_start: @quantile_setting.historical_start, historical_end: @quantile_setting.historical_end, period_id: @quantile_setting.period_id, predicted_end: @quantile_setting.predicted_end, predicted_start: @quantile_setting.predicted_start }
     assert_redirected_to quantile_setting_path(assigns(:quantile_setting))
   end
 
