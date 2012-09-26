1b349e4de20b6ef7727e4393916017d2af095635
Will Fisher <will@teknofire.net>
2012-09-26 14:39:18 -0800

    fixing script call with missing param

--- app/workers/make_reports.rb
+++ app/workers/make_reports.rb
@@ -7,9 +7,10 @@ class MakeReports
     report.start
     
     script = '/home/jiang/projects/quantilemapping/v3/draw_extreme_day_count.py'
+    data_dir = "/home/jiang/projects/quantilemapping/results/run_auto/"
     # SCRIPT      LIST OF TILES                        LON     LAT     TITLE            LOW TEMP   HIGH TEMP
     # #{script} "obs_YYYY_YYYY.tif adj_YYYY_YYYY.tif"  213.75  53.75   "Gulf of Alaska" 2.0        15.0
-    cmd = [script, "\"#{report.column1_file} #{report.column2_file}\"", report.location.lon, report.location.lat, "\"#{report.location.name}\"", report.low_temp_threshold, report.high_temp_threshold]
+    cmd = [script, data_dir, "\"#{report.column1_file} #{report.column2_file}\"", report.location.lon, report.location.lat, "\"#{report.location.name}\"", report.low_temp_threshold, report.high_temp_threshold]
     
     puts "Running: #{cmd.join(' ')}"
     puts `#{cmd.join(' ')}`  
