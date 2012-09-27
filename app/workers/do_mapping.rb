72167f6ed9625fb4975fdfb56a33cd3fec83267e
jiang Zhu <jiang@gina.alaska.edu>
2012-09-26 17:01:55 -0800

    take out year and algo

--- app/workers/do_mapping.rb
+++ app/workers/do_mapping.rb
@@ -7,8 +7,8 @@ class DoMapping
     settings.start
     # do the stuff to run the command here
     script = '/home/jiang/projects/quantilemapping/v3/quantile_mapping_auto.py'
-    cmd = [script, settings.historical_start, settings.historical_end, settings.predicted_start, settings.predicted_end, settings.period.name, settings.algorithm]
-    #script.py 1910 2001 2030 2060 Yearly 0
+    cmd = [script, settings.historical_start, settings.historical_end, settings.predicted_start, settings.predicted_end]
+    #script.py 1910 2001 2030 2060 
     
     puts "Running: #{cmd.join(' ')}"
     puts `#{cmd.join(' ')}`  
