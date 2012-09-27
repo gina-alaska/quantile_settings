539bf6cfc232f26352cdc9d01619703ead59e426
Will Fisher <will@teknofire.net>
2012-09-26 16:12:15 -0800

    -- is the solution

--- app/workers/make_reports.rb
+++ app/workers/make_reports.rb
@@ -10,8 +10,8 @@ class MakeReports
     data_dir = "/home/jiang/projects/quantilemapping/results/run_auto/"
     # SCRIPT      LIST OF TILES                        LON     LAT     TITLE            LOW TEMP   HIGH TEMP
     # #{script} "obs_YYYY_YYYY.tif adj_YYYY_YYYY.tif"  213.75  53.75   "Gulf of Alaska" 2.0        15.0
-    cmd = [script, data_dir, "\"#{report.column1_file} #{report.column2_file}\"", report.location.lon, report.location.lat, "\"#{report.location.name}\"", report.low_temp_threshold, report.high_temp_threshold]
-    cmd = cmd.join(' ').gsub('-', '\-')
+    cmd = [script, '--', data_dir, "\"#{report.column1_file} #{report.column2_file}\"", report.location.lon, report.location.lat, "\"#{report.location.name}\"", report.low_temp_threshold, report.high_temp_threshold]
+    cmd = cmd.join(' ')
     
     puts "Running: #{cmd}"
     puts `#{cmd}` 
